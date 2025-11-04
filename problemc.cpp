d9af3a7a65bcdcf9147033eace8669fb10e3eb7d
quollcucumber <rgithub@greatcactus.org>
2025-11-05 10:11:46 +1100

    Create problemc.cpp

---
+++ problemc.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using  namespace std;
+#define int long long
+set<int> factors(int num) {
+    set<int> s;
+    for(int i = 1; i<= num ; i++) {
+        if(num % i == 0) s.insert(i);
+    }
+    return s;
+}
+signed main() {
+    int num = 36;
+    set<int> s = factors(num);
+    int total = 0;
+    // go through all factors of num, and see what other edges it touches;
+    for(int i : s) {
+        for(int j : s) {
+            if(j > i && j % i == 0) {
+                total++;
+            }
+        }
+    }
+    cout<<total<<'\n';
+}
